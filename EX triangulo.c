ac705c0e7838676d92abe7880f6429265ba80db7
Leonardo Jesus <jleo66592@gmail.com>
2026-08-04 09:36:57 -0300

    Create EX triangulo.c

---
+++ EX triangulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int base,altura;
+	float area;
+	
+	base = 10;
+	altura = 20;
+	
+	area = (base*altura)/2;
+	
+	printf("A area do triangulo Retangulo de Base = %d e altura = %d e %f", altura, area, base);
+	
+	return 0;
+}
